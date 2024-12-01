c88e1c7f35c3abb11c489469e29d714e5c6a4c53
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2024-12-01 12:33:36 +0100

    interpreter implement storing block

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,4 @@
-use crate::{Graph, LogicBlock, Node};
+use crate::{Graph, LogicBlock, Node, StoringBlock};
 use std::fs;
 
 pub fn init_map(path: &str) -> Graph {
@@ -7,16 +7,33 @@ pub fn init_map(path: &str) -> Graph {
     let mut nodes = Vec::new();
     for (i, line) in lines.enumerate() {
         let parameters = line.split_whitespace().collect::<Vec<&str>>();
-        assert!(parameters.len() >= 2, "line {i} is not valid:\n{line}");
-        let node_id = parameters[0].parse::<u32>().unwrap();
-        let requirements = u8::from_str_radix(parameters[1], 2).unwrap();
-        let children = parameters
-            .iter()
-            .skip(2)
-            .map(|x| x.parse::<u32>().unwrap())
-            .collect();
-        let node = Node::LogicBlock(LogicBlock::new(requirements, children));
-        nodes.push((node, node_id));
+
+        let is_logical_block = &parameters[0][0..=0] != "^";
+        if is_logical_block {
+            assert!(parameters.len() >= 2, "line {i} is not valid:\n{line}");
+            let node_id = parameters[0].parse::<u32>().unwrap();
+
+            let requirements = u8::from_str_radix(parameters[1], 2).unwrap();
+            let children = parameters
+                .iter()
+                .skip(2)
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect();
+            let node = Node::LogicBlock(LogicBlock::new(requirements, children));
+            nodes.push((node, node_id));
+        } else {
+            assert!(parameters.len() >= 3, "line {i} is not valid:\n{line}");
+            let node_id = parameters[0][1..].parse::<u32>().unwrap();
+            let button = parameters[1].parse().unwrap();
+            let source = parameters[2].parse().unwrap();
+            let children = parameters
+                .iter()
+                .skip(3)
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect();
+            let node = Node::StoringBlock(StoringBlock::new(false, source, button, children));
+            nodes.push((node, node_id));
+        }
     }
     let mut graph = Graph::new();
     graph.insert_nodes(nodes);
@@ -29,7 +46,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_adder() {
         let mut map = init_map("./components/adder.bwc");
         // init input 1 to 96 (0b01100000)
         map.turn_on_lamp(6);
@@ -48,5 +65,63 @@ mod tests {
         assert!(!map.get_node(22).unwrap().is_on());
         assert!(!map.get_node(23).unwrap().is_on());
         assert!(map.get_node(24).unwrap().is_on());
+    }
+
+    #[test]
+    fn test_saver() {
+        let mut map = init_map("./components/saver.bwc");
+        // init input 1 to 96 (0b01100000)
+        map.turn_on_lamp(6);
+        map.turn_on_lamp(7);
+
+        // init input 2 to true
+        map.turn_on_lamp(9);
+
+        map.apply_changes();
+
+        println!("{:#?}", map);
+        // check output is 96 (0b01100000)
+        assert!(!map.get_node(10).unwrap().is_on());
+        assert!(!map.get_node(11).unwrap().is_on());
+        assert!(!map.get_node(12).unwrap().is_on());
+        assert!(!map.get_node(13).unwrap().is_on());
+        assert!(!map.get_node(14).unwrap().is_on());
+        assert!(map.get_node(15).unwrap().is_on());
+        assert!(map.get_node(16).unwrap().is_on());
+        assert!(!map.get_node(17).unwrap().is_on());
+
+        // set input 1 to 0 (0b00000000)
+        map.turn_off_lamp(6);
+        map.turn_off_lamp(7);
+
+        // set input 2 to false
+        map.turn_off_lamp(9);
+
+        map.apply_changes();
+
+        // check output is 96 (0b01100000)
+        assert!(!map.get_node(10).unwrap().is_on());
+        assert!(!map.get_node(11).unwrap().is_on());
+        assert!(!map.get_node(12).unwrap().is_on());
+        assert!(!map.get_node(13).unwrap().is_on());
+        assert!(!map.get_node(14).unwrap().is_on());
+        assert!(map.get_node(15).unwrap().is_on());
+        assert!(map.get_node(16).unwrap().is_on());
+        assert!(!map.get_node(17).unwrap().is_on());
+
+        // set input 2 to true
+        map.turn_on_lamp(9);
+
+        map.apply_changes();
+
+        // check output is 0 (0b00000000)
+        assert!(!map.get_node(10).unwrap().is_on());
+        assert!(!map.get_node(11).unwrap().is_on());
+        assert!(!map.get_node(12).unwrap().is_on());
+        assert!(!map.get_node(13).unwrap().is_on());
+        assert!(!map.get_node(14).unwrap().is_on());
+        assert!(!map.get_node(15).unwrap().is_on());
+        assert!(!map.get_node(16).unwrap().is_on());
+        assert!(!map.get_node(17).unwrap().is_on());
     }
 }
